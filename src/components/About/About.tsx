43af5c7959e20911c9e113e2edbb6386002f24ec
Dominic Pelino <pelinodominic@gmail.com>
2022-05-30 15:10:25 -0700

    make mobile ready

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -8,7 +8,7 @@ import Typography from '@mui/material/Typography';
 import  ImageList  from '@mui/material/ImageList';
 import  ImageListItem  from '@mui/material/ImageListItem';
 
-import { Nav } from '../Home';
+import { Nav, Overlay, BackgroundPic, Root, Main } from '../Home';
 //  Background Image
 import code_image from '../../assets/images/code.jpg';
 
@@ -21,57 +21,29 @@ import spiritlake_image from '../../assets/images/spiritlake.jpg';
 import enchant_image from '../../assets/images/enchant.jpeg';
 
 //  Styles
-const Root = styled("div")({
-    padding: 0,
-    margin: 0
-});
-const AboutMain = styled('main')( {
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${code_image});`,
-    width: '100%',
-    height: '100%',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    position: 'absolute',
-    zIndex: '-1'
-});
-const Overlay = styled('main')( {
-    backgroundColor: 'black',
-    opacity: '50%',
-    width: '30%',
-    height: '100%',
-    backgroundSize: '50%',
-    backgroundRepeat: 'no-repeat',
-    position: 'absolute',
-    zIndex: '0'
-});
 const AboutText = styled('div')({
-    width: '400px',
+    width: '100%',
     textAlign: 'center',
-    position: 'absolute',
-    top: '60%',
-    left: '15%',
-    transform: 'translate(-50%, -50%)',
-    color: 'rgb(46,125,50)',
-    zIndex: '1'
+    position: 'relative',
+    top: '10%'
 });
 const MainDescription = styled('div')({
     display: 'flex',
     flexDirection: 'column',
-    margin: '25px'
+    margin: '10px'
 });
 const PhotoWall = styled('div')({
     // opacity: '50%',
-    position: 'absolute',
-    top: '4%',
-    right: '4%',
+    position: 'relative',
+    top: '10%'
 });
 
 export const About = () => {
     return (
         <Root>
 {/* Left Text Section */}
-            <AboutMain>
+            <Main>
+                <BackgroundPic />
                 <Overlay/>
                 <Nav/>
                 <AboutText>
@@ -102,34 +74,34 @@ export const About = () => {
                         </Typography>
                     </MainDescription>
                 </AboutText>
-            </AboutMain>
 {/* Right Photo Section */}
-            <PhotoWall>
-                <ImageList
-                    sx={{ width: 1200, height: 925 }}
-                    variant="quilted"
-                    cols={4}
-                    rowHeight={150}>
-                        <ImageListItem cols={2} rows={3}>
-                            <img src={`${sbsit_image}`}/>
-                        </ImageListItem>
-                        <ImageListItem cols={2} rows={2}>
-                            <img src={`${spiritlake_image}`}/>
-                        </ImageListItem>
-                        <ImageListItem cols={1} rows={2}>
-                            <img src={`${backrope_image}`}/>
-                        </ImageListItem>
-                        <ImageListItem cols={1} rows={2}>
-                            <img src={`${canyoneer_image}`}/>
-                        </ImageListItem>
-                        <ImageListItem cols={2} rows={3}>
-                            <img src={`${enchant_image}`}/>
-                        </ImageListItem>
-                        <ImageListItem cols={2} rows={2}>
-                            <img src={`${helens_image}`}/>
-                        </ImageListItem>
-                </ImageList>
-            </PhotoWall>
+                <PhotoWall>
+                    <ImageList
+                        sx={{ width: 1, height: 1 }}
+                        variant="quilted"
+                        cols={4}
+                        rowHeight={100}>
+                            <ImageListItem cols={2} rows={3}>
+                                <img src={`${sbsit_image}`}/>
+                            </ImageListItem>
+                            <ImageListItem cols={2} rows={2}>
+                                <img src={`${spiritlake_image}`}/>
+                            </ImageListItem>
+                            <ImageListItem cols={1} rows={2}>
+                                <img src={`${backrope_image}`}/>
+                            </ImageListItem>
+                            <ImageListItem cols={1} rows={2}>
+                                <img src={`${canyoneer_image}`}/>
+                            </ImageListItem>
+                            <ImageListItem cols={2} rows={3}>
+                                <img src={`${enchant_image}`}/>
+                            </ImageListItem>
+                            <ImageListItem cols={2} rows={2}>
+                                <img src={`${helens_image}`}/>
+                            </ImageListItem>
+                    </ImageList>
+                </PhotoWall>
+            </Main>
         </Root>
     )
 };
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -18,10 +18,6 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import HomeIcon from '@mui/icons-material/Home';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 //  Background Image
 import code_image from '../../assets/images/code.jpg';
@@ -32,25 +28,26 @@ import hero_image from '../../assets/images/hero.jpg';
 //  Nav Styles
 const AllHIghIcons = styled ('div')({
     textAlign: 'center',
-    position: 'absolute',
+    position: 'relative',
     top: '10%',
-    left: '15%',
-    transform: 'translate(-50%)',
-    padding: '10px'
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    margin: '10px',
 });
 const HighIcons = styled('div')({
     textAlign: 'center',
     display: 'inline-flex',
-    margin: '25px',
+    // margin: '25px',
     position: 'relative',
+    padding: '5px',
 });
 
 //  Home Styles
-const Root = styled("div")({
+export const Root = styled("div")({
     padding: 0,
     margin: 0
 });
-const Main = styled('main')( {
+export const Main = styled('main')( {
     backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${code_image});`,
     width: '100%',
     height: '100%',
@@ -60,66 +57,73 @@ const Main = styled('main')( {
     position: 'absolute',
     zIndex: '-1'
 });
+export const BackgroundPic = styled('main')( {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${code_image});`,
+    width: '100%',
+    height: '100%',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    position: 'absolute',
+    top: '100%',
+    zIndex: '-1'
+});
 export const Overlay = styled('main')( {
     backgroundColor: 'black',
-    opacity: '50%',
-    width: '30%',
-    height: '100%',
-    backgroundSize: '50%',
+    opacity: '20%',
+    width: '100%',
+    height: '200%',
+    backgroundSize: '100%',
     backgroundRepeat: 'no-repeat',
     position: 'absolute',
     zIndex: '0'
 });
 const MainText = styled('div')({
-    width: '400px',
+    width: '100%',
     textAlign: 'center',
-    position: 'absolute',
-    top: '50%',
-    left: '15%',
+    position: 'relative',
+    top: '40%',
+    left: '50%',
     transform: 'translate(-50%, -50%)',
     color: 'white',
-    padding: '10px'
+    padding: '5px',
 });
 const AllLowIcons = styled ('div')({
     textAlign: 'center',
-    position: 'absolute',
-    bottom: '10%',
-    left: '15%',
-    transform: 'translate(-50%)',
-    padding: '10px'
+    position: 'relative',
+    top: '25%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
 });
 const LowIcons = styled('div')({
     textAlign: 'center',
     display: 'inline-flex',
-    margin: '25px',
+    // margin: '25px',
     position: 'relative',
+    padding: '5px',
 });
 
 //  HomePage Styles
-const HelloBox = styled('div')({
-    width: 'auto',
-    position: 'absolute',
-    top: '27%',
-    left: '60%',
-    transform: 'translate(-50%, -50%)',
-});
+
 const TextBox = styled('div')({
-    width: '400px',
-    position: 'absolute',
+    width: 'auto',
+    position: 'relative',
     top: '30%',
-    left: '60%',
+    left: '50%',
     transform: 'translate(-50%)',
 });
 const SkillsBox = styled('div')({
-    position: 'absolute',
-    bottom: '15%',
-    left: '60%',
+    position: 'relative',
+    top: '35%',
+    left: '50%',
     transform: 'translate(-50%)',
 });
 const OverlayTwo = styled ('div')({
     backgroundColor: 'black',
-    opacity: '60%',
-    width: '100%',
+    opacity: '50%',
+    width: '35%',
+    left: '50%',
+    transform: 'translate(-50%)',
     height: '100%',
     backgroundSize: '50%',
     backgroundRepeat: 'no-repeat',
@@ -144,13 +148,13 @@ export const Nav = () => {
                     </IconButton>
                 </Tooltip>
             </HighIcons>
-            <HighIcons>
+            {/* <HighIcons>
                 <Tooltip title="Resume Page" placement='bottom' arrow>
                     <IconButton component={Link} to='/resume' >
                         <InsertDriveFileIcon color='secondary' sx={{ fontSize: 75 }} />
                     </IconButton>
                 </Tooltip>
-            </HighIcons>
+            </HighIcons> */}
         </AllHIghIcons>
     );
 };
@@ -203,32 +207,28 @@ export const HeroMenu = () => {
 const HomePage = () => {
     return (
         <>
-            <HelloBox>
-                <OverlayTwo/>
-                <Typography variant="h2" align="center" color="secondary">
-                    Hello!
-                </Typography>
-            </HelloBox>
-            <TextBox>
-                <Typography variant="h5" align="center" color="white" paragraph>
-                    <OverlayTwo/>
-                    I created this webpage to showcase who I am, and what I like to do.
-                    Please look around and feel free to contact me if you have any questions.
-                </Typography>
-            </TextBox>
-            <SkillsBox>
-                <Typography variant="h2" align="center" color="secondary">
-                    <OverlayTwo/>
-                    Skills
-                </Typography>
-                <Typography variant="h5" align="center" color="white" paragraph>
-                    <OverlayTwo/>
-                    <p>Python, JavaScript, TypeScript</p>
-                    <p>Express, Flask, GraphQL, Node</p>
-                    <p>React, Redux, custom-hooks, REST APIs</p> 
-                    <p>PostgreSQL, Firebase, MongoDB, PGAdmin4</p> 
-                </Typography>
-            </SkillsBox>
+        <TextBox>
+        {/* <OverlayTwo/> */}
+            <Typography variant="h6" align="center" color="white" paragraph>
+                I created this webpage to showcase who I am, and what I like to do.
+            </Typography>
+            <Typography variant="h6" align="center" color="white" paragraph>
+                Please look around and feel free to contact me if you have any questions.
+            </Typography>
+        </TextBox>
+        <SkillsBox>
+            <Typography variant="h4" align="center" color="secondary">
+                {/* <OverlayTwo/> */}
+                Skills
+            </Typography>
+            <Typography variant="h6" align="center" color="white" paragraph>
+                {/* <OverlayTwo/> */}
+                <p>Python, JavaScript, TypeScript</p>
+                <p>Express, Flask, GraphQL, Node</p>
+                <p>React, Redux, custom-hooks, REST APIs</p> 
+                <p>PostgreSQL, Firebase, MongoDB, PGAdmin4</p> 
+            </Typography>
+        </SkillsBox>
         </>
     );
 };
@@ -237,6 +237,7 @@ export const Home = () => {
     return (
         <Root>
             <Main>
+                <BackgroundPic/>
                 <Overlay/>
                 <Nav/>
                 <HeroMenu/>
